0d73604a26dd080846162ad2d1a0f6b4b65b9496
Dima Solovei <38838582+disolovei@users.noreply.github.com>
2022-12-21 17:49:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 (function(script,wrapper,d,w){
     console.log(script.parentElement);
     console.log(w.location.href);
+
+    fetch('https://disolovei.github.io/embed-data/template.html')
+        .then(response => response.text())
+        .then(template => wrapper.innerHTML = template)
+        .catch(console.warn);
 })(document.currentScript,document.currentScript.parentElement,document,window);
