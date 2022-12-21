a55d7bee75e5c44dad4f4893235aecd6d5fbcb67
Dima Solovei <38838582+disolovei@users.noreply.github.com>
2022-12-21 15:57:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,5 @@
     console.dir({d, w, script});
     console.log(new URL(w.location.href));
     console.log(w.parent);
+    console.log(new URL(w.parent.location.href));
 })(document, window, document.currentScript);
